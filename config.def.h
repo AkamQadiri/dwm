243bdcdfbb9e0ecc7d11384f2706e8da25418b4e
AkamQadiri <akamq@hotmail.com>
2022-10-20 06:36:46 +0200

    Applied functionalgaps+pertag patch

--- config.def.h
+++ config.def.h
@@ -2,6 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const int startwithgaps[]    = { 1 };	/* 1 means gaps are used by default, this can be customized for each tag */
+static const unsigned int gappx[]   = { 10 };   /* default gap between windows in pixels, this can be customized for each tag */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -104,6 +106,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_g,      setgaps,        {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_g,      setgaps,        {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_g,      setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_b,      setgaps,        {.i = GAP_TOGGLE} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
