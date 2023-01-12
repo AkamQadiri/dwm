32b8b9a72e3431f5e4c1ab2d639b105f560e6ab8
AkamQadiri <akamq@hotmail.com>
2023-01-12 12:29:33 +0100

    Updated layout symbols

--- config.def.h
+++ config.def.h
@@ -61,8 +61,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
     /* symbol   arrange function */
-    { "T",      tile }, /* first entry is default */
-    { "F",      NULL }, /* no layout function means floating behavior */
+    { "[T]",    tile }, /* first entry is default */
+    { "[F]",    NULL }, /* no layout function means floating behavior */
     { "[M]",    monocle },
 };
 
